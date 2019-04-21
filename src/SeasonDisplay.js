3602ec30d12f05979b043861651b200b846ca966
Fadli Ramadhan <fadlir93@gmail.com>
2019-04-21 17:25:56 +0700

    Determining Season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'Summer' : 'Winter'
+    } else {
+        return lat > 0 ? 'Winter' : 'Summer'
+    }
+}
+
 const SeasonDisplay = (props) => {
-    console.log(props.lat)
+    const season = getSeason(props.lat, new Date().getMonth());
+    console.log(season)
     return <div>Season Display</div>
 };
 
