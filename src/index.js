256c779041d9cb583e356912b29b4f75b877e40f
Fadli Ramadhan <fadlir93@gmail.com>
2019-04-21 17:19:53 +0700

    Passing state as props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    state = { test: null}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ coord: position.coords.latitude}),
+            err => this.setState({errorMessage : err.message})
+        )        
+    }
+
+    render() {
+        if(this.state.errorMessage && !this.state.coord) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.coord){
+            return <SeasonDisplay lat={this.state.coord}/>;
+        } 
+        return <div>Loading!</div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
